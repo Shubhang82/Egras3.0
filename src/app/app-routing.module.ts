d5c353f6683bdfcf526835cbc9fc88216080ec33
cpsingh022 <108063263+cpsingh022@users.noreply.github.com>
2022-09-20 15:07:01 +0530

    add forgot password

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { ChallanComponent } from './challan/challan.component';
 import { RegisterComponent } from './register/register.component';
 import { SignupComponent } from './signup/signup/signup.component';
 import { TesttestComponent } from './testtest/testtest.component';
-
+import { ForgotPasswordComponent } from './password-recovery/forgot-password/forgot-password.component';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./login/login/login-routing.module').then(m => m.LoginRoutingModule) },
@@ -12,8 +12,9 @@ const routes: Routes = [
   { path: 'Test', component: TesttestComponent },
   {path: 'Signup', component: SignupComponent},
   // { path: '', loadChildren: () => import('./default/default-routing.module').then(m => m.DefaultRoutingModule) },
+  {path: 'ForgotPassword', component: ForgotPasswordComponent},
   { path: '**', redirectTo: '' },
-
+ 
 
   {
     path: 'Profile',
